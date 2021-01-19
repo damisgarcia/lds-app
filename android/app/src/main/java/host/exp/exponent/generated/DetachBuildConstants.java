94e132e20a97511f47d8bff2925bd48d2b90b763
Damis Garcia <damisgarcia@gmail.com>
2021-01-19 14:20:55 -0300

    atualizado sdk android para 29

--- android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -3,6 +3,5 @@ package host.exp.exponent.generated;
 // This file is auto-generated. Please don't rename!
 public class DetachBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "exp2853272907524d4390e0621b5962deda://192.168.1.64:19000";
-
+  public static final String DEVELOPMENT_URL = "exp2853272907524d4390e0621b5962deda://192.168.1.64:19000";  
 }
